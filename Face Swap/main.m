1b66f160fa26b2239f922c325c02ea9f008977cc
capitan <capitan@bogon>
2016-07-31 09:17:44 -0700

    Initial Commit

---
+++ Face Swap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Face Swap
+//
+//  Created by capitan on 7/31/16.
+//  Copyright © 2016 greenmango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
